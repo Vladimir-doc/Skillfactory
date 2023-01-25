b4eac8897faf8709221cb17bec5bd203f1df44d2
Vladimir-doc <118751685+Vladimir-doc@users.noreply.github.com>
2023-01-25 21:11:06 +0300

    ИТОГОВОЕ ЗАДАНИЕ 3.6 (HW-03)

---
+++ NewsPaper/news/views.py
@@ -0,0 +1,17 @@
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'news.html'
+    context_object_name = 'news'
+    ordering = 'dateCreation'
+
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'new.html'
+    context_object_name = 'new'
