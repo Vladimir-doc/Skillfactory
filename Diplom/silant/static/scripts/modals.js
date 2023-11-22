c5ffa0465f08818ff0ed8296da21211b00ba1671
Vladimir-doc <118751685+Vladimir-doc@users.noreply.github.com>
2023-11-23 00:34:15 +0300

    Delete Diplom/silant directory

--- Diplom/silant/static/scripts/modals.js
+++
@@ -1,6 +0,0 @@
-function ModalsWindows(){
-    $(".modal-area,.modal-exit,.cancel-js").click(function(){
-        $(this).closest("section").fadeOut();
-        return false;
-    });
-}
--- Diplom/silant/techservice/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class TechserviceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'techservice'
-    verbose_name = 'Сервисное обслуживание'
